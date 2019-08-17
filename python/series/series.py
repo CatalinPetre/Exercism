97e9c559f54249c455cc47c23d881f96c4a36715
CatalinPetre <petrec@Catalins-MBP.home>
2019-08-17 10:09:27 +0100

    Completed series exercise

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,9 @@
 def slices(series, length):
-    pass
+    results = []
+    if len(series) == 0 or length <= 0 or len(series) < length:
+        raise ValueError("Please try again. The values do not fit \
+        the purpose of the exercise")
+    for i in range(0, len(series) - (length - 1)):
+        my_slice = series[i:i+length]
+        results.append(my_slice)
+    return results
