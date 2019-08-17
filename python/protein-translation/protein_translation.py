4c3f090603c6d4860f92823b61164941243e3f40
CatalinPetre <petrec@Catalins-MBP.home>
2019-08-17 22:36:49 +0100

    Completed protein-translation exercise

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,17 @@
 def proteins(strand):
-    pass
+    my_dict = dict(AUG='Methionine', UUU='Phenylalanine',
+                   UUC='Phenylalanine', UUA='Leucine', UUG='Leucine',
+                   UCU='Serine', UCC='Serine', UCA='Serine',
+                   UCG='Serine', UAU='Tyrosine', UAC='Tyrosine',
+                   UGU='Cysteine', UGC='Cysteine', UGG='Tryptophan',
+                   UAA='STOP', UAG='STOP', UGA='STOP')
+    list = []
+    i = 0
+    while i < len(strand):
+        if strand[i:i+3] != 'UAA' and strand[i:i+3] != 'UAG' \
+           and strand[i:i+3] != 'UGA':
+            list.append(my_dict.get(strand[i:i+3]))
+        else:
+            break
+        i = i+3
+    return list
