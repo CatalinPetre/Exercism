99a468a61abdbb72df69cb18dc1db0ce1d9c05cb
CatalinPetre <petrec@Catalins-MBP.home>
2019-08-17 10:08:20 +0100

    Completed largest-series-product

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -1,7 +1,16 @@
-def largest_product(series, size):
-    largest_prod = 0
-    for i in series:
-        prod = series(i) * series(i+1) * series(i+2)
-        if prod > largest_prod:
-            largest_prod = prod
-        i = i + 1
+def largest_product(series, length):
+    prod_list = []
+    if length == 0:
+        return 1
+    elif len(series) == 0 or length < 0 or len(series) < length:
+        raise ValueError("The series needs to have more than one element, \
+        the size needs to be a positive number and shorter than \
+        the length of the series. Please try again.")
+    for i in range(0, len(series) - (length - 1)):
+        my_slice = series[i:i+length]
+        int_my_slice = [int(j) for j in my_slice]
+        prod = 1
+        for i in int_my_slice:
+            prod = prod * i
+        prod_list.append(prod)
+    return max(prod_list)
