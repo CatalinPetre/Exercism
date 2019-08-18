4fdd1cd505f7b821b0ba7b5f2d9fcbf8faa5ffcb
CatalinPetre <petrec@Catalins-MBP.home>
2019-08-18 01:30:05 +0100

    Updated largest-series-product exercise after Tom's feedback

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -1,5 +1,5 @@
-def largest_product(series, length):
-    prod_list = []
+def slices(series, length):
+    results = []
     if length == 0:
         return 1
     elif len(series) == 0 or length < 0 or len(series) < length:
@@ -8,6 +8,16 @@ def largest_product(series, length):
         the length of the series. Please try again.")
     for i in range(0, len(series) - (length - 1)):
         my_slice = series[i:i+length]
+        results.append(my_slice)
+    return results
+
+
+def largest_product(series, length):
+    results = slices(series, length)
+    if results == 1:
+        return 1
+    prod_list = []
+    for my_slice in results:
         int_my_slice = [int(j) for j in my_slice]
         prod = 1
         for i in int_my_slice:
